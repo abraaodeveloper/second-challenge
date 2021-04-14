395b2955c7970b0b5d7a56636e5d3dd2cad22a29
abraaodeveloper <abraaobarbosati@gmail.com>
2021-04-14 11:25:48 -0300

    bug movimentação dos players

--- js/Jogador.js
+++ js/Jogador.js
@@ -3,9 +3,10 @@ export default class Jogador {
     posicaoOndeIr = 0;
     refTabuleiro;
     callback;
-    passo = 3;
-    passosASeguir = 0;
-    passosAVoltar = 0;
+    passo = 5;
+    qtdPassosVoltando = 0;
+    qtdPassosIndo = 0;
+    podeMover = false;
 
     constructor(nome, img, tabuleiro) {
         this.x = -1;
@@ -21,25 +22,20 @@ export default class Jogador {
         this.tabuleiro = tabuleiro;
     }
 
-    moverAte(qtdDePassos, callback) {
+    moverAte(numeroCasa, callback) {
         this.callback = callback;
         if (this.posicao === -1) {
             this.posicao = 0;
             this.x = this.refTabuleiro[0].x;
             this.y = this.refTabuleiro[0].y;
         }
+        this.posicaoOndeIr = numeroCasa;
+        this.qtdPassosIndo = this.posicaoOndeIr - this.posicao;
 
-        let qtdPassosDeIr = 0;
-        for (let local = this.posicao; local < this.refTabuleiro.length && local < this.posicao+qtdDePassos; local++)
-            qtdPassosDeIr++;
-
-        this.passosASeguir = qtdPassosDeIr;
-        this.passosAVoltar = qtdDePassos - qtdPassosDeIr;
-
-        if(this.posicao + this.passosASeguir < this.refTabuleiro.length){
-            this.posicaoOndeIr = this.posicao + this.passosASeguir;
-        }else if(this.passosAVoltar > 0){
-            this.posicaoOndeIr = this.posicao + this.passosAVoltar;
+        this.podeMover = true;
+        if(this.refTabuleiro.lenght <= this.posicaoOndeIr){
+            this.qtdPassosVoltando = (this.refTabuleiro.lenght -1) - this.posicao;
+            this.qtdPassosVoltando = this.posicaoOndeIr - (this.refTabuleiro.lenght -1);// ver se esse -1 esta coreeto 
         }
     }
 
@@ -52,26 +48,31 @@ export default class Jogador {
             this.posicao = this.posicao + 1;
             this.x = this.refTabuleiro[this.posicao + 1].x;
             this.y = this.refTabuleiro[this.posicao + 1].y;
-            //setTimeout(()=>{}, 1000)
+
+            if(this.qtdPassosIndo > 0){
+                this.qtdPassosIndo--;
+            }else if(this.qtdPassosVoltando > 0){
+                this.qtdPassosVoltando--;
+            }
+            if (this.posicao === this.posicaoOndeIr) {
+
+                this.podeMover = false;
+                console.log(this.posicaoOndeIr+" - "+this.posicao);
+                if (this.callback != null) this.callback();
+                this.callback = null;
+          
+                return;
+            }
         }
-
-        if (this.x === this.refTabuleiro[this.posicaoOndeIr].x
-            && this.y === this.refTabuleiro[this.posicaoOndeIr].y) {
-
-            if(this.posicao === this.posicaoOndeIr)
-            if (this.callback != null) this.callback();
-            this.callback = null;
-            return;
+        
+        console.log("pode mover: "+ this.podeMover);
+        if(!this.podeMover){
+            if(this.qtdPassosIndo > 0){
+                this._moverParafrente();
+            }else if(this.qtdPassosVoltando > 0){
+                this._moverParaAtras();
+            }
         }
-
-        if(this.passosASeguir != 0){
-            this._moverParafrente();
-            this.passosASeguir--;
-        }else if(this.passosAVoltar != 0){
-            this._moverParaAtras();
-            this.passosAVoltar--;
-        }
-
         /*
         if (this.x === this.refTabuleiro[this.posicao + 1].x
             && this.y === this.refTabuleiro[this.posicao + 1].y) {
