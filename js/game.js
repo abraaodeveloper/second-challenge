87aa4cd124cf855554a1ee8b706469e0ab8c450e
abraaodeveloper <abraaobarbosati@gmail.com>
2021-04-14 11:37:48 -0300

    Correção de erros na movimentação

--- js/game.js
+++ js/game.js
@@ -30,7 +30,11 @@ for (const btn of btnsJogar) {
         jogar = true;
     });
 }
+const imgP1 = new Image();
+imgP1.src = '././imgs/player1.png';
 
+const imgP2 = new Image();
+imgP2.src = '././imgs/player2.png';
 
 function frameUpdate() {
     context.drawImage(background, 0, 0, tamanhoGame, tamanhoGame);
@@ -43,19 +47,17 @@ function frameUpdate() {
 
     // movimentação do jogador 1
     if (cobrasEscadas.jogador1.posicao != -1) {
-        context.drawImage(cobrasEscadas.jogador1.img, cobrasEscadas.jogador1.x,
+        context.drawImage(imgP1, cobrasEscadas.jogador1.x,
             cobrasEscadas.jogador1.y, tamanho, tamanho);
     }
     if (cobrasEscadas.jogador1.podeMover) cobrasEscadas.jogador1.mover();
 
     // movimentação do jogador 2
     if (cobrasEscadas.jogador2.posicao != -1) {
-        context.drawImage(cobrasEscadas.jogador2.img, cobrasEscadas.jogador2.x,
+        context.drawImage(imgP2, cobrasEscadas.jogador2.x,
             cobrasEscadas.jogador2.y, tamanho, tamanho);
     }
     if (cobrasEscadas.jogador2.podeMover) cobrasEscadas.jogador2.mover();
-
-
 
     // debug
     /*
